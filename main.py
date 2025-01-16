be304ff8691da3c30c9536ba0d8bd536ec7e2aca
Egorkud <egorkud2005@gmail.com>
2025-01-17 00:35:23 +0200

    Initialisation of sheets in main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+import openpyxl
+import json
 
 from pathlib import Path
 from openpyxl.workbook import Workbook
@@ -8,12 +10,45 @@ from instruments import data_instruments as DI
 from instruments import data_scrappers as DS
 from instruments import  import_generators as IG
 
+# Initialisation of sheets
+try:
+    export_file = openpyxl.open("name.xlsx", False)  # Main file to work, Prom export table
+    models_book = openpyxl.load_workbook("name.xlsx")  # Auto filled table (articule, engl, ru, ukr)
+    data_changes = config.data_changes
+    # region Sheet initialisaion
+    with open("data/links_data.json", "r", encoding="utf-8") as file:
+        links_data = json.load(file)
+
+    export_sheet = export_file.active
+
+    blank_book = openpyxl.open("data/Empty file for auto seat case.xlsx", False)  # Empty blank for any tasks, flexible
+    blank_products = blank_book["Export Products Sheet"]
+    blank_groups = blank_book["Export Groups Sheet"]
+
+    book_empty = openpyxl.Workbook() # Empty table
+    book_empty.remove(book_empty["Sheet"])
+    empty_sheet = book_empty.create_sheet("Export Products Sheet")
+    new_groups_sheet = book_empty.create_sheet("Export Groups Sheet")
+
+    models_sheet = models_book["Export Products Sheet"]
+    groups_sheet = models_book["Export Groups Sheet"]
+    # endregion
+except Exception as ex:
+    print(ex)
+    raise SystemExit
+
 
 def main():
     start = time.time()
     DI.init_project() # Initialises all dirs and files for work
 
 
+    # region Closing sheets
+    export_file.close()
+    models_book.close()
+    book_empty.close()
+    blank_book.close()
+    # endregion
 
     print(f"\nTime elapsed: {time.time() - start} seconds")
 
