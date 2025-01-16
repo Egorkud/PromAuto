92f663032f3e843c573b69f9015dce3aabe29410
Egorkud <egorkud2005@gmail.com>
2025-01-16 17:48:46 +0200

    Init main file with time counter

---
+++ main.py
@@ -0,0 +1,13 @@
+import json
+import openpyxl
+import time
+
+
+def main():
+    start = time.time()
+
+
+    print(f"\nTime elapsed: {time.time() - start} seconds")
+
+if __name__ == '__main__':
+    main()
